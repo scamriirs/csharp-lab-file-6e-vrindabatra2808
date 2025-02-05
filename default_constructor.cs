86c3e4ddb72840148a045fb82d39bf2748a70a41
vrindabatra2808 <150940057+vrindabatra2808@users.noreply.github.com>
2025-02-05 12:10:24 +0530

    Create default_constructor.cs

---
+++ default_constructor.cs
@@ -0,0 +1,14 @@
+//Default constructor
+using System;
+public class Student
+    {
+        int studentid;
+        string studentname;
+        public static void Main()
+        {
+            Student s1=new Student();
+            Console.WriteLine(s1.studentid);
+            Console.WriteLine(s1.studentname);
+        }
+    
+    }
